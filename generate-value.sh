e7bc08204bc9bc9adb4b3647a59a2860b3f53698
ay-buildkite <amaury@buildkite.com>
2025-01-29 21:10:36 -0500

    Update generate-value.sh

--- generate-value.sh
+++ generate-value.sh
@@ -2,12 +2,4 @@
 set -euo pipefail  # Enable strict error handling
 
 value=$(openssl rand -hex 12 2> /dev/null || date +%s | sha256sum | base64 | head -c 16)
-
-value=$(echo "$value" | tr -d '[:space:]')
-if [[ -z "$value" ]]; then
-  echo "Error: Generate value is empty!"
-  exit 1
-fi
-
 echo -n "$value" > output.txt
-echo -n "Generated value is $(cat output.txt)"
